9112f01972e997c544b7188a9d7f337ce47948f5
Hannah Elder <Hannah.Elder@vu.com>
2023-04-09 21:07:18 -0500

    v5

--- displayInventory.py
+++ displayInventory.py
@@ -2,6 +2,7 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(Inventory):
     # your code goes here
+    print('Inventory: ')
     for item in Inventory:
         print(Inventory[item], item)
 
