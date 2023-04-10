45a2e2b41135ead79f535b0e2cf3b3cdbc02a4b9
Hannah Elder <Hannah.Elder@vu.com>
2023-04-09 20:54:19 -0500

    v4

--- displayInventory.py
+++ displayInventory.py
@@ -1,11 +1,12 @@
+import pytest
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
     # your code goes here
-    for item in stuff:
-        print(stuff[item], item)
+    for item in inventory:
+        print(inventory[item], item)
 
-    item_cnt = sum(stuff[item] for item in stuff)
+    item_cnt = sum(inventory[item] for item in inventory)
     print(f"Total number of items: {item_cnt}")
 
 
